1db37de23cc2c4ea74de0f01dac58a6509c92455
Roland Michael Norbert Mutter <36150861+mutterroland@users.noreply.github.com>
2018-08-11 04:42:43 +0300

    Fix buggs in code. Add MainPage.py
    Fixed bug that duplicates some of the commits in OneRepo.py . Add MainPage.py script that generates a "main page" for our md pages.

--- tests/MainPage.py
+++ tests/MainPage.py
@@ -1,40 +1,48 @@
 import os
 import json
 import markdown2
-
 current_dir = os.path.dirname(os.path.realpath(__file__))
+
+def link_to_md( name ):
+    path = "file:///" + current_dir + "\\" + name + ".md"
+    erl = '''<a href="%s">link</a>''' % path
+    return erl
+
+
+
+
 
 json_data= open('./github_changelog.json').read()
 data = json.loads(json_data)
 
-repos = ['beetmoverscript']
+repos = ['Markdown Repo Table']
 
-base_table = "| Commit Number | Message | Data | \n" + \
-            "|:---:|:----------------------------------:|:----:| \n"
+base_table = "| Repo | Link | Last Message | Data | \n" + \
+            "|:---:|:----:|:----------------------------------:|:----:| \n"
 tables = {}
 for repo in repos:
     tables[repo] = base_table
 
 
-categories = data['beetmoverscript']
+for key in data:
+    arepo = key
+    link = link_to_md(key)
+    lastMsg = data[key]['0']['Message: ']
 
+    date = data[key]['0']["Date: "]
 
-for key, value in categories.items():
-    ncommit = key
-    lastMsg = categories[key]['Message: ']
-    date = categories[key]["Date: "]
-
-    row = "|" + ncommit +\
+    row = "|" + arepo +\
+        "|" + link +\
         "|" + lastMsg +\
         "|" + date + '\n'
-    #print(row)
+
     for repo in tables.keys():
-        #print (tables[repo])
+        print (tables.keys())
         tables[repo] = tables[repo] + row
 
-#print (tables.items())
+
 output_files = []
-md_file_name = "beetmoverscript.md"
+md_file_name = "Main.md"
 md_file = open(current_dir + "/../" + md_file_name, 'w')
 
 for key, value in tables.items():
